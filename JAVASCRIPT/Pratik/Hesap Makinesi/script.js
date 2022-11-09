0e8e6ccf40ff0a6feacd35cac84a1aaa129b5923
Baris UNAL <unallbrs@gmail.com>
2022-11-09 16:33:56 +0300

    Hesap Makinesi programı tamamlandı.

--- JAVASCRIPT/Pratik/Hesap Makinesi/script.js
+++ JAVASCRIPT/Pratik/Hesap Makinesi/script.js
@@ -2,6 +2,9 @@ const display = document.querySelector('.calculator-input');
 const keys = document.querySelector('.calculator-keys');
 
 let displayValue = '0';
+let firstValue = null;
+let operator = null;
+let waitingForSecondValue = false;
 
 updateDisplay();
 
@@ -11,39 +14,114 @@ function updateDisplay() {
 
 keys.addEventListener('click', function(e) {
     const element = e.target;
+    const value = element.value;
 
     if(!element.matches('button')) return;
 
-    if(element.classList.contains('operator')) {
-        console.log('operator', element.value);
-        return;
+    switch(value){
+        case '+':
+        case '-':
+        case '*':
+        case '/':
+        case '=':
+            handleOperator(value);
+            break;
+        case '.':
+            inputDecimal();
+            break;
+        case 'clear':
+            clear();
+            break;
+        default:
+            inputNumber(element.value);
     }
+    updateDisplay();
 
-    if(element.classList.contains('decimal')) {
-        // console.log('decimal', element.value);
-        inputDecimal();
-        updateDisplay();
-        return;
-    }
 
-    if(element.classList.contains('clear')) {
-        console.log('clear', element.value);
-        return;
-    }
+//Switch-Case kullanılarak yorum satırına alınan kodlar daha az kodla yazılmış oldu.
+    // if(element.classList.contains('operator')) {
+    //     // console.log('operator', element.value);
+    //     handleOperator(element.value);
+    //     updateDisplay();
+    //     return;
+    // }
+
+    // if(element.classList.contains('decimal')) {
+    //     // console.log('decimal', element.value);
+    //     inputDecimal();
+    //     updateDisplay();
+    //     return;
+    // }
+
+    // if(element.classList.contains('clear')) {
+    //     // console.log('clear', element.value);
+    //     clear();
+    //     updateDisplay();
+    //     return;
+    // }
 
     // console.log('number', element.value)
 
-    inputNumber(element.value);
-    updateDisplay();
+    // inputNumber(element.value);
+    // updateDisplay();
+//Switch-Case kullanılarak yorum satırına alınan kodlar daha az kodla yazılmış oldu.
 });
 
-function inputNumber(num) {
-    displayValue = displayValue=='0' ? num : displayValue + num ;
+function handleOperator(nextOperator) {
+    const value = parseFloat(displayValue);
+
+    if (operator && waitingForSecondValue) {
+        operator = nextOperator;
+        return;
+    }
+
+    if (firstValue === null) {
+        firstValue = value;
+    } else if (operator) {
+        const result = calculate(firstValue, value, operator);
+
+        displayValue = `${parseFloat(result.toFixed(7))}`;
+        firstValue = result;
+    }
+
+    waitingForSecondValue = true;
+    operator = nextOperator;
+
+    console.log(displayValue, firstValue, operator, waitingForSecondValue);
+
 }
 
-function inputDecimal(){
-    if(!displayValue.includes('')) {
-        displayValue += '.'; 
+function calculate(first, second, operator) {
+    if (operator=== '+') {
+        return first + second;
+    } else if (operator==='-'){
+        return first - second;
+    }else if (operator==='*'){
+        return first * second;
+    }else if (operator==='/'){
+        return first / second;
+    } 
+
+    return second;
+}
+
+function inputNumber(num) {
+    if (waitingForSecondValue){
+        displayValue = num;
+        waitingForSecondValue = false;
+    } else {
+    displayValue = displayValue=='0' ? num : displayValue + num ;
     }
-    
+
+    console.log(displayValue, firstValue, operator, waitingForSecondValue);
+} 
+
+function inputDecimal() {
+    if (!displayValue.includes('.')) {
+        displayValue += '.'; 
+    }    
+}
+
+function clear() {
+    displayValue = '0';
 }

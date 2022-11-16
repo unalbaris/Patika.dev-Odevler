bd11a275b12458bb03d48504ada601a79932594e
Baris UNAL <unallbrs@gmail.com>
2022-11-16 12:18:44 +0300

    Binary Calculator Tamamlandı.

--- JAVASCRIPT/Pratik/Binary Calculator/binaryCalculator.js
+++ JAVASCRIPT/Pratik/Binary Calculator/binaryCalculator.js
@@ -1,17 +1,7 @@
 const res = document.querySelector('#res');
 const btns = document.querySelector('#btns');
 
-let displayValue = '0';
-let firstValue = null;
-let operator = null;
-let waitingForSecondValue = false;
-
-updateDisplay();
-
-function updateDisplay() {
-    res.innerHTML = displayValue;
-}
-
+let displayValue = '';
 
 btns.addEventListener('click',function(e) {
     const value = e.target.value;
@@ -21,69 +11,83 @@ btns.addEventListener('click',function(e) {
         case('-'):
         case('*'):
         case('/'):
-        case('='):
-            handleOperator(value);
+        case('1'):
+        case('0'):
+            operator(value);
             break
+        case('='):
+            calculate(value);
+            break;
         case('C'):
             clear();
-            break;
-        default:
-            inputNumber(value);
+            break; 
     }
-  
-    updateDisplay();
+
 });
 
-function handleOperator(nextOperator) {
-    const value = displayValue;
-
-    if (operator && waitingForSecondValue) {
-        operator = nextOperator;
-        return;
-    }
-
-    if (firstValue === null) {
-        firstValue = value;
-    } else if (operator) {
-        const result = calculate (firstValue, value, operator);
-
-        displayValue = `${result}`;
-        firstValue = result;
-    }
-
-    waitingForSecondValue = true;
-    operator = nextOperator;
-
-    console.log(displayValue, firstValue, operator, waitingForSecondValue);
+function operator(num) {
+    displayValue += num;
+    res.innerHTML = displayValue;        
 }
 
-function calculate (first, second, operator) {
-    if (operator=== '+') {
-        return Number(first) + Number(second);
-    } else if (operator==='-'){
-        return first - second;
-    }else if (operator==='*'){
-        return first * second;
-    }else if (operator==='/'){
-        return first / second;
-    } 
-
-    return second;
-}
-
-
-function inputNumber(num) {
-    if (waitingForSecondValue) {
-        displayValue = num;
-        waitingForSecondValue = false;
-    } else {
-        displayValue = displayValue=='0' ? num : displayValue + num;
+function calculate () {
+    let getBeforeOperator;
+    let getAfterOperator;
+    if(displayValue.includes('+')){
+        getBeforeOperator = displayValue.slice(0, displayValue.indexOf('+')).split('').reverse();
+        getAfterOperator = displayValue.slice(displayValue.indexOf('+')+1,).split('').reverse();   
+        let beforeTotal = 0;
+        for (let i=0; i<getBeforeOperator.length; i++ ){
+            beforeTotal += getBeforeOperator[i]*Math.pow(2,i)
+        }
+        let afterTotal = 0;
+        for (let i=0; i<getAfterOperator.length; i++ ){
+            afterTotal += getAfterOperator[i]*Math.pow(2,i)
+        }
+        res.innerHTML = (beforeTotal+afterTotal).toString(2);
+    } else if(displayValue.includes('-')){
+        getBeforeOperator = displayValue.slice(0, displayValue.indexOf('-')).split('').reverse();
+        getAfterOperator = displayValue.slice(displayValue.indexOf('-')+1,).split('').reverse();  
+        let beforeTotal = 0;
+        for (let i=0; i<getBeforeOperator.length; i++ ){
+            beforeTotal += getBeforeOperator[i]*Math.pow(2,i)
+        }
+        let afterTotal = 0;
+        for (let i=0; i<getAfterOperator.length; i++ ){
+            afterTotal += getAfterOperator[i]*Math.pow(2,i)
+        }
+        res.innerHTML = (beforeTotal-afterTotal).toString(2); 
+    } else if(displayValue.includes('*')){
+        getBeforeOperator = displayValue.slice(0, displayValue.indexOf('*')).split('').reverse();
+        getAfterOperator = displayValue.slice(displayValue.indexOf('*')+1,).split('').reverse();   
+        let beforeTotal = 0;
+        for (let i=0; i<getBeforeOperator.length; i++ ){
+            beforeTotal += getBeforeOperator[i]*Math.pow(2,i)
+        }
+        let afterTotal = 0;
+        for (let i=0; i<getAfterOperator.length; i++ ){
+            afterTotal += getAfterOperator[i]*Math.pow(2,i)
+        }
+        res.innerHTML = (beforeTotal*afterTotal).toString(2);
+    } else if(displayValue.includes('/')){
+        getBeforeOperator = displayValue.slice(0, displayValue.indexOf('/')).split('').reverse();
+        getAfterOperator = displayValue.slice(displayValue.indexOf('/')+1,).split('').reverse(); 
+        let beforeTotal = 0;
+        for (let i=0; i<getBeforeOperator.length; i++ ){
+            beforeTotal += getBeforeOperator[i]*Math.pow(2,i)
+        }
+        let afterTotal = 0;
+        for (let i=0; i<getAfterOperator.length; i++ ){
+            afterTotal += getAfterOperator[i]*Math.pow(2,i)
+        }
+        res.innerHTML = (beforeTotal/afterTotal).toString(2);  
     }
+    
+    
 
-    console.log(displayValue, firstValue, operator, waitingForSecondValue);
 }
 
 function clear() {
-    displayValue = '0';
+   displayValue = ''; 
+   res.innerHTML = displayValue;
 }
-
